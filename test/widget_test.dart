290a4c9ffd3a625a5b5deb5e9213806fa6b91d42
john-muinde <66753112+john-muinde@users.noreply.github.com>
2024-03-14 00:04:19 +0300

    first commit of removing syntax and import errors

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:study_buddy_app/main.dart';
+import 'package:study_buddy_app/services/notification/model/notification_body.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(
+      body: NotificationBody(
+        orderId: 0,
+        type: 'notification',
+      ),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
